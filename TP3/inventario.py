a11aef60c1812ae836066cdcfa9a97cf816fbaf5
Nicolas <nicolas.ales@outlook.com>
2021-06-02 18:36:37 -0300

    update inventario.py

--- TP3/inventario.py
+++ TP3/inventario.py
@@ -1,82 +1,170 @@
 import simpy
 import numpy as np
 
+min_demanda = 1
+max_demanda = 5             # intervalo de valores enteros que puede tomar la demanda (distribucion uniforme)
+parametro_arribos = 5       # lamda de la distribucion exponencial de arribos de clientes
 
-def warehouse_run(env, order_cutoff, order_target):
-    global inventory, balance, num_ordered
-    inventory = order_target
+min_inventario = 50         # s: cantidad minima deseable de inventario
+max_inventario = 200        # S: cantidad maxima de unidades en inventario
+dias_entre_pedidos = 15     # Cada cuanto se hace un pedido
+cOrden = 80                 # Costo por ordenar una unidad
+cFaltante = 50              # Costo por unidad de no poder satisfacer la demanda requerida
+cMantenimiento = 5          # Costo de mantener en deposito una unidad
+valor_unidad = 100          # Valor al que se vende cada unidad
+
+
+def simulacion_inventario(env, min_inventario, max_inventario):
+    global inventario, balance, cant_ordenada, costo_de_faltante, costo_de_orden, costo_de_mantenimiento
+    inventario = max_inventario
     balance = 0.0
-    num_ordered = 0
+    cant_ordenada = 0
+    costo_de_faltante = 0
+    costo_de_orden = 0
+    costo_de_mantenimiento = 0
 
     while True:
-        interarrival = generate_interarrival()
-        yield env.timeout(interarrival)
-        balance -= inventory * 2 * interarrival
-        demand = generate_demand()
-        if demand < inventory:
-            balance += 100 * demand
-            inventory -= demand
-            print('{:.2f} sold {}'.format(env.now, demand))
+        tiempo_entre_arribo = generate_interarrival()
+        yield env.timeout(tiempo_entre_arribo)
+        demanda = generate_demand()
+        if demanda < inventario:
+            balance += valor_unidad * demanda
+            inventario -= demanda
+            print('{:.2f} vendido {}'.format(env.now, demanda))
         else:
-            balance += 100 * inventory
-            inventory = 0
-            print('{:.2f} sold {} (out of stock)'.format(env.now, inventory))
+            balance += 100 * inventario
+            costo_de_faltante -= cFaltante * (inventario - demanda)
+            inventario = 0
+            print('{:.2f} vendido {} (sin stock)'.format(env.now, inventario))
 
-        if inventory < order_cutoff and num_ordered == 0:
-            env.process(handle_order(env, order_target))
 
-def handle_order(env, order_target):
-    global inventory, balance, num_ordered
+def orden(env):
+    global inventario, min_inventario, max_inventario
+    while True:
+        env.process(hacer_orden(env, max_inventario))
+        yield env.timeout(dias_entre_pedidos)
 
-    num_ordered = order_target - inventory
-    print('{:.2f} placed order for {}'.format(env.now, num_ordered))
-    balance -= 50*num_ordered
+
+def hacer_orden(env, order_target):
+    global inventario, cant_ordenada, costo_de_orden
+
+    cant_ordenada = order_target - inventario
+    print('{:.2f} orden hecha por {}'.format(env.now, cant_ordenada))
+    costo_de_orden += cOrden * cant_ordenada
     yield env.timeout(2.0)
-    inventory += num_ordered
-    num_ordered = 0
-    print('{:.2f} received order, {} in inventory'.format(env.now, inventory))
+    inventario += cant_ordenada
+    cant_ordenada = 0
+    print('{:.2f} orden recibida, {} en inventario'.format(env.now, inventario))
 
 
 def generate_interarrival():
-    return np.random.exponential(1. / 5)
+    return np.random.exponential(1. / parametro_arribos)
 
 
 def generate_demand():
-    return np.random.randint(1, 5)
+    return np.random.randint(min_demanda,max_demanda)
+
 
 obs_time = []
 inventory_level = []
 balance_time = []
+costoF = []
+costoO = []
+costoM = []
+
 
 def observe(env):
-    global inventory, balance
+    global inventario, balance, costo_de_faltante, costo_de_orden, costo_de_mantenimiento
     while True:
+        costo_de_mantenimiento += 0.1 * cMantenimiento * inventario
         obs_time.append(env.now)
-        inventory_level.append(inventory)
+        inventory_level.append(inventario)
         balance_time.append(balance)
+        costoF.append(costo_de_faltante)
+        costoO.append(costo_de_orden)
+        costoM.append(costo_de_mantenimiento)
         yield env.timeout(0.1)
+
 
 np.random.seed(0)
 env = simpy.Environment()
-env.process(warehouse_run(env, 30, 50))
+env.process(simulacion_inventario(env, min_inventario, max_inventario))
+env.process(orden(env))
 env.process(observe(env))
-env.run(until=30.0)
+env.run(until=90.0)
+
+costoTotal = []
+balanceTotal = []
+for i, _ in enumerate(obs_time):
+    costo_total = costoF[i] + costoO[i] + costoM[i]
+    costoTotal.append(costo_total)
+    balance_total = balance_time[i] - costo_total
+    balanceTotal.append(balance_total)
+
+balancePos = []
+balanceNeg = []
+
+aux = True
+for b in balanceTotal:
+    if b >= 0 and aux == True:
+        balancePos.append(b)
+        balanceNeg.append(np.nan)
+        aux = True
+    elif b >= 0 and aux == False:
+        balancePos.append(b)
+        balanceNeg.append(b)
+        aux = False
+    elif b < 0 and aux == True:
+        balancePos.append(b)
+        balanceNeg.append(b)
+        aux = False
+    elif b < 0 and aux == False:
+        balancePos.append(np.nan)
+        balanceNeg.append(b)
 
 import matplotlib.pyplot as plt
 
-fig, (InvL,BlnT)= plt.subplots(2,sharex=True)
+fig1, (InvL, BlnT, CF, CO, CM) = plt.subplots(5, sharex=True)
 
-InvL.step(obs_time,inventory_level,where='post')
-InvL.set(xlabel='Simulation Time (days)',ylabel='Inventory level' )
+InvL.step(obs_time, inventory_level, where='post')
+InvL.set(xlabel='Simulation Time (days)', ylabel='Inventory level')
 InvL.label_outer()
 InvL.set_xlim(xmin=0)
 InvL.set_ylim(ymin=0)
 
-BlnT.step(obs_time,balance_time,where='post')
-BlnT.set(xlabel='Simulation Time (days)',ylabel='Balance' )
+BlnT.step(obs_time, balance_time, where='post', color='green')
+BlnT.set(xlabel='Simulation Time (days)', ylabel='Balance')
 BlnT.label_outer()
 BlnT.set_xlim(xmin=0)
 BlnT.set_ylim(ymin=0)
 
+CF.step(obs_time, costoF, where='post', color='red')
+CF.set(xlabel='Simulation Time (days)', ylabel='Costo Faltante')
+CF.label_outer()
+CF.set_xlim(xmin=0)
+
+CO.step(obs_time, costoO, where='post', color='red')
+CO.set(xlabel='Simulation Time (days)', ylabel='Costo Orden')
+CO.label_outer()
+CO.set_xlim(xmin=0)
+
+CM.step(obs_time, costoM, where='post', color='red')
+CM.set(xlabel='Simulation Time (days)', ylabel='Costo Mantenimiento')
+CM.label_outer()
+CM.set_xlim(xmin=0)
+
+fig2, (CT, Bal) = plt.subplots(2, sharex=True)
+
+CT.step(obs_time, costoTotal, where='post', color='red')
+CT.set(xlabel='Simulation Time (days)', ylabel='Costos Totales')
+CT.label_outer()
+CT.set_xlim(xmin=0)
+
+Bal.step(obs_time, balanceNeg, where='post', color='red')
+plt.axhline(0, color='black')
+Bal.step(obs_time, balancePos, where='post', color='green')
+Bal.set(xlabel='Simulation Time (days)', ylabel='Balance')
+Bal.label_outer()
+Bal.set_xlim(xmin=0)
 
 plt.show()
